ace262bd9795ab40fd07e0b4dcbc033667eba005
HarshMagiya7 <harshmagiya0702@gmail.com>
2023-01-04 18:25:03 +0000

    GTS branding

--- branding/hooks.py
+++ branding/hooks.py
@@ -12,7 +12,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/branding/css/novacept.css"
-# app_include_js = "/assets/branding/js/branding.js"
+app_include_js = "/assets/branding/js/novacept.js"
 
 # include js, css files in header of web template
 web_include_css = "/assets/branding/css/login.css"
