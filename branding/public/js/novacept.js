c3c9decbea77796037ecfb2856609aa91f9030a6
HarshMagiya7 <harshmagiya0702@gmail.com>
2023-01-05 19:04:48 +0000

    Changes in CSS

--- branding/public/js/novacept.js
+++ branding/public/js/novacept.js
@@ -1,5 +1,5 @@
 $(window).on('load', function() {
     frappe.after_ajax(function () {
-        $(`<div class="novaBrandingClass"><p class="novaBrandingText">Powered by</p> <a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/R6BfWV4N/oie-j-PCa-Sj-Vh-QRAE.png" alt="logo" width="85"></a></div>`).insertAfter("#navbar-breadcrumbs") 
+        $(`<div class="novaBrandingClass"><p class="novaBrandingText">Powered by</p> <a class="novaBrandingAttribute" href="https://novacept.io/" target="_blank"><img class="novaBrandingImage" src="https://i.postimg.cc/R6BfWV4N/oie-j-PCa-Sj-Vh-QRAE.png" alt="logo" width="80"></a></div>`).insertAfter("#navbar-breadcrumbs") 
     })
 })
